7f98ea49fa40b6bc0aebb4e54d095c458ed1a33a
Gibbsforce <75646733+Gibbsforce@users.noreply.github.com>
2020-12-30 13:33:00 -0500

    Add files via upload

---
+++ contact/contact_script.js
@@ -0,0 +1,23 @@
+const firstNavBarCel = () => {
+
+	const firstNavBarCelButton = document.querySelector(".firstnavbar-cel-button");
+	const firstNavBarLinks = document.querySelector(".firstnavbar-links");
+	const navCelLinks = document.querySelectorAll(".firstnavbar-links li");
+
+	firstNavBarCelButton.addEventListener("click", () => {
+
+		firstNavBarLinks.classList.toggle("firstnavbar-active");
+
+		navCelLinks.forEach((link, index) => {
+
+			if(link.style.animation) {
+				link.style.animation = "";
+			} else {
+				link.style.animation = `fadingNavCel 0.5s ease forwards ${index / 7 + 0.5}s`;
+			}
+		});
+		firstNavBarCelButton.classList.toggle("toggle");	
+	});
+}
+
+firstNavBarCel();
