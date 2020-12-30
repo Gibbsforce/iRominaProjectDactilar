416046f32d39a7de95e70b18010449b49160f532
Gibbsforce <75646733+Gibbsforce@users.noreply.github.com>
2020-12-30 13:44:55 -0500

    Add files via upload

--- shop/main_shop_script.js
+++ shop/main_shop_script.js
@@ -9,66 +9,89 @@ const hatsWeb1 = document.querySelector("#hats-web-1");
 const scarves0 = document.querySelector("#scarves-web-0");
 const scarves1 = document.querySelector("#scarves-web-1");
 const scarves2 = document.querySelector("#scarves-web-2");
+const scarves3 = document.querySelector("#scarves-web-3");
 const jewelryWeb = document.querySelector("#jewelry-web");
 
 function removeHats () {
+
 	hatsWeb0.style.display = "none";
 	hatsWeb1.style.display = "none";
+
 };
 
 function removeScarves () {
+
 	scarves0.style.display = "none";
 	scarves1.style.display = "none";
 	scarves2.style.display = "none";
+	scarves3.style.display = "none";
+
 };
 
 function removeJewelryWeb () {
+
 	jewelryWeb.style.display = "none";
+
 };
 
 function addHats () {
+
 	hatsWeb0.style.display = "block";
 	hatsWeb1.style.display = "block";
+
 };
 
 function addScarves () {
+
 	scarves0.style.display = "block";
 	scarves1.style.display = "block";
 	scarves2.style.display = "block";
+	scarves3.style.display = "block";
+
 };
 
 function addJewelryWeb () {
+
 	jewelryWeb.style.display = "block";
+
 };
 
 all.addEventListener("click", () => {
+
 	addHats ();
 	addScarves ();
 	addJewelryWeb ();
+
 });
 
 hats.addEventListener("click", () => {
+
 	addHats ();
 	addJewelryWeb ();
 	removeJewelryWeb ();
 	addScarves ();
 	removeScarves ();
+
 });
 
 scarves.addEventListener("click", () => {
+
 	addScarves ();
 	addHats ();
 	removeHats ();
 	addJewelryWeb ();
 	removeJewelryWeb ();
+
 });
 
 jewelry.addEventListener("click", () => {
+
 	addJewelryWeb ();
 	addHats ();
 	removeHats ();
 	addScarves ();
 	removeScarves ();
+
 });
 
 const firstNavBarCel = () => {
@@ -89,8 +112,28 @@ const firstNavBarCel = () => {
 				link.style.animation = `fadingNavCel 0.5s ease forwards ${index / 7 + 0.5}s`;
 			}
 		});
+
 		firstNavBarCelButton.classList.toggle("toggle");	
 	});
 }
 
-firstNavBarCel();
+const secondNavBarCel = () => {
+
+const category = document.querySelector(".category");
+const categoryCel = document.querySelector(".category-cel");
+
+	category.addEventListener("click", () => {
+
+		categoryCel.classList.toggle("category-cel-active");
+
+	});
+}
+
+const shop = () => {
+
+	firstNavBarCel();
+	secondNavBarCel();
+
+}
+
+shop();
